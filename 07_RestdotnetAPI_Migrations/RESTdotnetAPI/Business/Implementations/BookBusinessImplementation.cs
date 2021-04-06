1842fe1d573382577b82f2c73c9f5924ea593f8b
Alexsander Alves Cordeiro <alexsander.cordeiro@itbit.com.br>
2021-04-06 17:48:33 -0300

    Adicao das migrations com sql

---
+++ 07_RestdotnetAPI_Migrations/RESTdotnetAPI/Business/Implementations/BookBusinessImplementation.cs
@@ -0,0 +1,51 @@
+﻿using RESTdotnetAPI.Model;
+using RESTdotnetAPI.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RESTdotnetAPI.Business.Implementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IBookRepository _repository;
+
+        private MySQLContext _context;
+
+        public BookBusinessImplementation(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        //Find Methods....
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Book FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+
+            
+        }
+
+        public Book Update(Book book)
+        {
+
+            return _repository.Update(book);
+        } 
+    }
+}
---
+++ 07_RestdotnetAPI_Migrations/RESTdotnetAPI/Repository/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using RESTdotnetAPI.Model;
+using System.Collections.Generic;
+
+
+namespace RESTdotnetAPI.Business
+
+{
+    public interface IBookRepository
+    {
+        Book Create(Book book);
+        Book FindByID(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+        bool Exists(long id);
+     }
+}
